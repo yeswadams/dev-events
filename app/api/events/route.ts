7762911ac6d776928215e74e2ca5eb6911787bfc
yeswadams <yeswaadams73@gmail.com>
2026-09-18 03:37:18 +0300

    Implemented a GET api endpoint to fetch all the events from our db

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,7 +1,8 @@
 import connectDB from "@/lib/mongodb";
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 import { NextRequest, NextResponse } from "next/server";
 import { Event } from "@/database/index";
+import { tryLoadManifestWithRetries } from "next/dist/server/load-components";
 
 export async function POST(req: NextRequest) {
   try {
@@ -11,33 +12,52 @@ export async function POST(req: NextRequest) {
 
     let event;
     try {
-        event = Object.fromEntries(formData.entries());
-    } catch(e) {
-        return NextResponse.json(
-          {
-            message: 'Invalid JSON data format', 
-            error: e instanceof Error ? e.message : "Failed to create the Event"
-          }, 
-          { status: 400 }
-        );
-    };
+      event = Object.fromEntries(formData.entries());
+    } catch (e) {
+      return NextResponse.json(
+        {
+          message: "Invalid JSON data format",
+          error: e instanceof Error ? e.message : "Failed to create the Event",
+        },
+        { status: 400 },
+      );
+    }
 
-    const file = formData.get('image')  as File;
+    const file = formData.get("image") as File;
 
-    if(!file) {
-      return NextResponse.json({ message: 'Image file is required'}, {status: 400})
-    };
+    if (!file) {
+      return NextResponse.json(
+        { message: "Image file is required" },
+        { status: 400 },
+      );
+    }
 
-    const createdEvent = await Event.create(event);
-    
+    const arrayBuffer = await file.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
 
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { resource_type: "image", folder: "devEvent" },
+          (error, results) => {
+            if (error) return reject(error);
+
+            resolve(results);
+          },
+        )
+        .end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
+
+    const createdEvent = await Event.create(event);
+
     return NextResponse.json(
-      { 
-        message: 'Event created successfully', 
-        event: createdEvent 
-      }, 
-      { status: 201 }
+      {
+        message: "Event created successfully",
+        event: createdEvent,
+      },
+      { status: 201 },
     );
   } catch (e) {
     console.log(e);
@@ -48,5 +68,29 @@ export async function POST(req: NextRequest) {
       },
       { status: 500 },
     );
-  };
-};
+  }
+}
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const events = await Event.find().sort({ createdAt: -1 }); // the latests events appear at the top
+
+    return NextResponse.json(
+      {
+        message: "Events fetched successfully ",
+        events,
+      },
+      { status: 200 },
+    );
+  } catch (e) {
+    return NextResponse.json(
+      {
+        message: " Event fetching failed",
+        error: e,
+      },
+      { status: 500 },
+    );
+  }
+}
--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -11,7 +11,6 @@ type MongooseCache = {
 
 // Extend the global object to include our mongoose cache
 declare global {
-   // eslint-disable-next-line no-var
   var mongoose: MongooseCache | undefined;
 }
 
